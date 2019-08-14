d51b9e03beca3db2bd56d234ed7ee69a949cfa5b
chad187 <chad187@gmail.com>
2019-08-14 16:43:04 -0700

    fixed again and confirmed to work on PC

--- transparentScreen/controller.js
+++ transparentScreen/controller.js
@@ -6,7 +6,6 @@ let dropZone = document.getElementById('image')
 
 ipcRenderer.on('show-prompt', (event, text, side, id) => {
 	let splitter = os.platform() == 'darwin' ? "/" : "\\"
-	if (os.platform() == 'darwin')
   dialogs.prompt('Give it a name', text.slice(0, -4).split(splitter).pop(), ok => {
 		ipcRenderer.send('return-prompt', ok, side, id)
 	})
